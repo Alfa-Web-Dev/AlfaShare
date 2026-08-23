bfa238d94e577bac47cc29df9973943f15845b5b
Alfa-Web-Dev <aarifnawaz1000@gmail.com>
2026-08-23 11:40:25 +0530

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='alfashare-v4';
+const CACHE='alfashare-v4-1';
 const CORE=['/','/index.html','/css/style.css','/js/app.js','/manifest.json','/icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
